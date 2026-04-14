96e3a72fcb9c71bc4d3c9104e79da9f15b0d93c0
aguslasalvia <agus.blumenfeld13@gmail.com>
2026-04-14 11:22:02 -0300

    Program.cs

--- Universidad.Web/Program.cs
+++ Universidad.Web/Program.cs
@@ -1,49 +1,56 @@
-using Universidad.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Universidad.Infrastructure.Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=university.db"));
-
-builder.Services.AddControllers();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        options.ExpireTimeSpan = TimeSpan.FromHours(8);
-        options.SlidingExpiration = true;
-    });
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthorization();
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite("Data Source=university.db")
+        );
 
-var app = builder.Build();
+        builder.Services.AddControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+        builder
+            .Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromHours(8);
+                options.SlidingExpiration = true;
+            });
+
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.EnsureCreated();
+        }
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseHsts();
+            builder.WebHost.UseUrls("http://0.0.0.0:5000");
+        }
+        else
+        {
+            builder.WebHost.UseUrls("http://localhost:5000");
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
+    }
 }
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-    builder.WebHost.UseUrls("http://0.0.0.0:5000");
-}
-else
-{
-    builder.WebHost.UseUrls("http://localhost:5000");
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
-app.Run();
